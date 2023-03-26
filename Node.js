0dc3d14ac7507d132811a46e912d04bb6ac27fa6
Achudan TS <36720465+Achudan@users.noreply.github.com>
2023-03-26 04:29:25 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Allow cross-origin requests
 app.use(cors());
